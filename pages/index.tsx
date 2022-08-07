07e03509091ca76b462330b439ecc32be8bef8de
michael <kimh060612@naver.com>
2022-08-07 23:42:35 +0900

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,62 @@
+import * as React from 'react';
 import type { NextPage } from "next";
+
+import Layout from '../components/layout/Layout';
+import ArrowLink from '../components/link/ArrowLink';
+import ButtonLink from '../components/link/ButtonLink';
+import UnderlineLink from '../components/link/UnderlineLink';
+import UnstyledLink from '../components/link/UnstyledLink';
+
+import Vercel from '../public/vercel.svg';
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-blue-500"> Hello </h1>
-    </div>
+    <Layout>
+
+      <main>
+        <section className='bg-white'>
+          <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
+            <Vercel className='text-5xl' />
+            <h1 className='mt-4'>
+              Next.js + Tailwind CSS + TypeScript Starter
+            </h1>
+            <p className='mt-2 text-sm text-gray-800'>
+              A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
+              Import, Seo, Link component, pre-configured with Husky{' '}
+            </p>
+            <p className='mt-2 text-sm text-gray-700'>
+              <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
+                See the repository
+              </ArrowLink>
+            </p>
+
+            <ButtonLink className='mt-6' href='/components' variant='light'>
+              See all components
+            </ButtonLink>
+
+            <UnstyledLink
+              href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
+              className='mt-4'
+            >
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                width='92'
+                height='32'
+                src='https://vercel.com/button'
+                alt='Deploy with Vercel'
+              />
+            </UnstyledLink>
+
+            <footer className='absolute bottom-2 text-gray-700'>
+              © {new Date().getFullYear()} By{' '}
+              <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
+                Theodorus Clarence
+              </UnderlineLink>
+            </footer>
+          </div>
+        </section>
+      </main>
+    </Layout>
   );
 };
 
