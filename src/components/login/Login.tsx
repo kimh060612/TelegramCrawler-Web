e4b1a3f50e4081ea6168f0afeb195477668ad12a
michael <kimh060612@naver.com>
2022-08-22 22:53:26 +0900

    .

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -5,8 +5,6 @@ import Layout from "../layout/Layout";
 import LoginInputContainer from '../input/LoginInput';
 import ButtonComponent from '../button/LoginButton';
 
-
-
 interface Props {}
 
 const LoginContainer: NextPage<Props> = () => {
@@ -14,13 +12,24 @@ const LoginContainer: NextPage<Props> = () => {
     const [ invalidLoginMessage, setInvalidLoginMessage ] = useState<string>('');
 
     async function submitForm(username: string, password: string) {
-
+        
     }
 
     return (
         <Layout>
             <div className="bg-white shadow-2xl rounded px-8 pt-6 pb-8 mb-4 flex flex-col w-screen max-w-sm sm:max-w-md">
                 <h3 className="text-gray-500 text-center uppercase font-bold mb-2">Login</h3>
+                <LoginInputContainer
+                    message="Enter your Email"
+                    onChange={}
+                    errorMessage={}
+                    required
+                    id="username"
+                    label="email"
+                    name="username"
+                    placeholder="Enter Your Email"
+                / >
+
             </div>
         </Layout>
     );
